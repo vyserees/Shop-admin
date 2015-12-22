ee5127d5b333ef1c117d014862a72110c681a1e6
Vladimir Veljkovic <vyserees@gmail.com>
2015-12-22 13:09:32 +0100

    Config

--- app/config/config.php
+++ app/config/config.php
@@ -1,8 +1,8 @@
 <?php
 
 $auxilium = array();
-$auxilium['APP_NAME'] = 'MVC';
-$auxilium['APP_URL'] = 'http://www.mvc.dev/';
+$auxilium['APP_NAME'] = 'Mixer';
+$auxilium['APP_URL'] = 'http://www.shopadmin.dev/';
 $auxilium['APP_TIMEZONE'] = 'Europe/Belgrade';
 //$auxilium['DEBUG'] = TRUE;
 $auxilium['ROUTER'] = TRUE;
--- app/config/db.php
+++ app/config/db.php
@@ -2,9 +2,9 @@
 $db = array();
 //----conection-------
 $db['DB_HOST'] = 'localhost';
-$db['DB_NAME'] = '';
-$db['DB_USER'] = '';
-$db['DB_PASS'] = '';
+$db['DB_NAME'] = 'shopadmin';
+$db['DB_USER'] = 'root';
+$db['DB_PASS'] = '123test';
 //-----details------
 $db['DB_TYPE'] = 'PDO';
 $db['DB_CHARSET'] = 'utf8';
